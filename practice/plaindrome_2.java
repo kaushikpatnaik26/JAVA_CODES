82ca45ad68904ccdf612b92ace7c0776e76e5a16
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2025-01-25 20:59:03 +0530

    Update plaindrome_2.java

--- practice/plaindrome_2.java
+++ practice/plaindrome_2.java
@@ -1,14 +1,12 @@
 package practice;
-
 import java.util.*;
-
 public class palin {
 
     public static boolean isPalindrome(String x) {
         int left = 0;
         int right = x.length() - 1;
 
-        while (left > right) {
+        while (left < right) {
             if (x.charAt(left) != x.charAt(right)) {
                 return false;
             }
@@ -22,10 +20,11 @@ public class palin {
         Scanner sc = new Scanner(System.in);
         System.out.print("Entere a string/number to check if it is a Palindrome or not:  ");
         String val = sc.nextLine();
-        isPalindrome(val);
+        
         if (isPalindrome(val)) {
             System.out.println("The Entered string is a palindrome");
-        } else {
+        }
+        else {
             System.out.println("The entered string is not a plaindrome");
         }
         sc.close();
